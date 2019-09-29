561013c029592c0b1f713734f5a05f9c11baa4cf
pandabear100 <nelsonysun@gmail.com>
2019-09-29 15:03:13 -0700

    Changed AutoCompetition to inherit from MasterAutonomous, and added the test method vuforiaFollowObject() to runOpMode.

--- Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/AutoCompetition.java
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/AutoCompetition.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.team6220_2019;
 
-public class AutoCompetition extends MasterTeleOp
+public class AutoCompetition extends MasterAutonomous
 {
     @Override
     public void runOpMode() throws InterruptedException
     {
+        initialize();
 
+        while(opModeIsActive()) {
+            vuforiaFollowObject();
+
+            telemetry.update();
+            idle();
+        }
     }
 }
