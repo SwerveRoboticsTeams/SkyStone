4cec5f62c10139a4b4cf09508d17a579a27b564f
pandabear100 <nelsonysun@gmail.com>
2019-09-29 15:44:19 -0700

    Fixed bug in AutoCompetition that rendered it undiscoverable.

--- Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/AutoCompetition.java
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/AutoCompetition.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.team6220_2019;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "AutoCompetition")
 public class AutoCompetition extends MasterAutonomous
 {
     @Override
