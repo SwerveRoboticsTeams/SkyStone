5eb6c6ddabe830c8b3b6c0797bc2ccfd51647349
Justin Nguyen <fignewtojn@gmail.com>
2019-10-17 19:08:09 -0700

    Created framwork and created vuforia test for skystones

---
+++ Team8923_2019/src/main/java/org/firstinspires/ftc/team8923_2019/MasterTeleOp.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.team8923_2019;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+abstract class MasterTeleOp extends Master
+{
+    public void driveMecanumTeleOp()
+    {
+        // Reverse drive if desired
+        if(gamepad1.right_stick_button)
+            reverseDrive = false;
+        if(gamepad1.left_stick_button)
+            reverseDrive = true;
+
+        if(gamepad1.dpad_down)
+            slowModeDivisor = 3.0;
+        else if(gamepad1.dpad_up)
+            slowModeDivisor = 1.0;
+
+        double y = -gamepad1.left_stick_y; // Y axis is negative when up
+        double x = 0.5 * gamepad1.left_stick_x;
+        double power = calculateDistance(x, y);
+        double turnPower = -gamepad1.right_stick_x; // Fix for clockwise being a negative rotation
+        double angle = Math.toDegrees(Math.atan2(-x, y)); // 0 degrees is forward
+
+        driveMecanum(angle, power, turnPower);
+    }
+
+    double calculateDistance(double deltaX, double deltaY)
+    {
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    }
+
+    void sendTelemetry()
+    {
+
+        telemetry.addData("left stick x:", gamepad1.left_stick_x);
+        telemetry.addData("left stick y:", gamepad1.left_stick_y);
+        telemetry.addData("right stick x:", gamepad1.right_stick_x);
+        telemetry.addData("right stick y:", gamepad1.right_stick_y);
+        telemetry.addData("right trigger:", gamepad2.right_trigger);
+        telemetry.addData("left trigger:", gamepad2.left_trigger);
+        telemetry.update();
+    }
+}
---
+++ Team8923_2019/src/main/java/org/firstinspires/ftc/team8923_2019/TeleOpCompetition.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.team8923_2019 ;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "TeleOp Competition")
+
+public class TeleOpCompetition extends MasterTeleOp
+{
+    @Override
+    public void runOpMode()
+    {
+        initHardware();
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            driveMecanumTeleOp();
+            sendTelemetry();
+            idle();
+        }
+    }
+}
