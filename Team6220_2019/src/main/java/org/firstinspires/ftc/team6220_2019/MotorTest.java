34fc7e18cf3b1d06a3df0f1b7a6879f4c1ee3248
Cole Welch <colewelch151@gmail.com>
2019-10-22 08:59:16 -0700

    Changed functionality of MotorTest to test RUN_TO_POSITION.  It looks like the implementation of this mode has changed this year.

--- Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/MotorTest.java
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/MotorTest.java
@@ -6,25 +6,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Motor Test")
-public class MotorTest extends LinearOpMode
+public class MotorTest extends MasterOpMode
 {
     @Override
     public void runOpMode() throws InterruptedException
     {
+        // Add gamepad to callback---------------
+        driver1 = new DriverInput(gamepad1);
+        callback.add(driver1);
+
+        int newTargetPos = 5000;
+
+
         DcMotor motor = hardwareMap.dcMotor.get("motor");
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor.setPower(0);
+        // Have to set this BEFORE RUN_TO_POSITION now
+        motor.setTargetPosition(newTargetPos);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         waitForStart();
+        // Accounts for delay between initializing the program and starting TeleOp
+        lTime = timer.seconds();
 
-        while (opModeIsActive())
+        motor.setPower(0.6);
+
+        while(/*motor.isBusy()*/ opModeIsActive())
         {
-            motor.setPower(gamepad1.right_stick_y);
+            // Finds the time elapsed each loop
+            double eTime = timer.seconds() - lTime;
+            lTime = timer.seconds();
+
+
+            if (driver1.isButtonJustPressed(Button.DPAD_UP))
+            {
+                newTargetPos += 1000;
+                motor.setTargetPosition(newTargetPos);
+            }
+            else if (driver1.isButtonJustPressed(Button.DPAD_DOWN))
+            {
+                newTargetPos -= 1000;
+                motor.setTargetPosition(newTargetPos);
+            }
+
 
             telemetry.addData("Motor Power: ", motor.getPower());
             telemetry.addData("Motor Position: ", motor.getCurrentPosition());
+            updateCallback(eTime);
             telemetry.update();
             idle();
         }
