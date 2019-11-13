f79558951734b928ef1fb685e0c41bdec8d62054
Cole Welch <colewelch151@gmail.com>
2019-11-12 22:10:53 -0800

    Tested Vuforia navigation between successive points, found that targets can be easily lost.  Added (untested) encoder backup navigation to keep robot moving at least until targets are found again.

--- Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/TestAuto.java
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/TestAuto.java
@@ -12,39 +12,29 @@ import java.util.Scanner;
 public class TestAuto extends MasterAutonomous
 {
 
-    public int[][] navPoints = {{-18, -18},
-            {-18, 18},
-            {-18, 18},
-            {-18, -18}};
+    public int[][] navPoints = {{-36, -36},
+            {-36, 36},
+            {-36, -36},
+            {-36, 36}};
 
     @Override
     public void runOpMode() throws InterruptedException
     {
         initialize(true, false);
 
-        setRobotStartingOrientation(0);
+        setRobotStartingOrientation(90);
 
         vRes.getLocation();
 
         waitForStart();
 
-
-        // todo Test!
-        // Position = a couple inches in front of red #2 VuMark
         driveToCoordinates(-35, -45, 90, 0.3);
 
-        // todo Test once driveToCoordinates seems to work
-        /*int targetNum = 0;
-
-        if (driveToCoordinates(navPoints[targetNum][0], navPoints[targetNum][1], 0))
+        /*// todo Test!
+        // Position = a couple inches in front of red #2 VuMark
+        for(int i = 0; i < navPoints.length; i++)
         {
-            if (targetNum < navPoints.length - 1)
-            {
-                targetNum++;
-            } else
-            {
-                break;
-            }
+            driveToCoordinates(navPoints[i][0], navPoints[i][1], 90, 0.3);
         }*/
 
 
