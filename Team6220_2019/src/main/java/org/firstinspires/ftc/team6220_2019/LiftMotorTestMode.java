e0236fd026e5d26cd68019005f22f24d27600d6f
pandabear100 <nelsonysun@gmail.com>
2019-12-16 18:26:39 -0800

    These classes are just designed to test the lift motors.

---
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/LiftMotorTestMode.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.team6220_2019;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract public class LiftMotorTestMode extends LinearOpMode {
+    DcMotor liftMotor0, liftMotor1, liftMotor2;
+
+    DriverInput driver;
+    List<ConcurrentOperation> callback;
+
+    ElapsedTime timer = new ElapsedTime();
+
+    public void initialize(){
+        driver = new DriverInput(gamepad1);
+
+        callback = new ArrayList<>();
+
+        liftMotor0 = hardwareMap.dcMotor.get("liftMotor0");
+        liftMotor1 = hardwareMap.dcMotor.get("liftMotor1");
+        liftMotor2 = hardwareMap.dcMotor.get("liftMotor2");
+
+        liftMotor0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        liftMotor0.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        callback.add(driver);
+    }
+
+    public void updateCallback(double eTime)
+    {
+        for (ConcurrentOperation item : callback)
+        {
+            item.update(eTime);
+        }
+    }
+}
