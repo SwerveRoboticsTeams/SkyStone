19f32bfa02d7f7a561b806c30b8e77c48c179ad3
Cole Welch <colewelch151@gmail.com>
2019-12-20 17:59:01 -0800

    More work on odometry, still need to figure out how to implement simulator for this year's field.

--- Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/OdometryNavigation.java
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/OdometryNavigation.java
@@ -8,8 +8,23 @@ public abstract class OdometryNavigation extends MasterOpMode
         double globalYPos = 0;
         double globalAngle = 0;
 
-        // Need to get both absolute and relative orientations of robot.
+        // Need to get distance and angle between robot and target.
+        double distanceToTarget = Math.hypot(x-globalXPos, y-globalYPos);
         double absoluteAngleToTarget = Math.atan2(y - globalYPos, x - globalXPos);
         double relativeAngleToTarget = normalizeAngle(absoluteAngleToTarget - getAngularOrientationWithOffset());
+
+        // Calculate relative x and y distances to our target.
+        double relativeXToPoint = Math.cos(relativeAngleToTarget) * distanceToTarget;
+        double relativeYToPoint = Math.sin(relativeAngleToTarget) * distanceToTarget;
+
+        // Calculate movement x and y powers, which must be normalized to prevent excessively large
+        // or small power values.
+        double movementXPower = relativeXToPoint / (Math.abs(relativeXToPoint) + Math.abs(relativeYToPoint));
+        double movementYPower = relativeYToPoint / (Math.abs(relativeXToPoint) + Math.abs(relativeYToPoint));
+
+        // todo Where are movement_x and movement_y supposed to be initialized?  Also, how does this integrate with simulator?
+        // todo Also, size of field = 140.94 in, not 144 in.  (Should probably adjust for this value...)
+        /*movement_x = movementXPower;
+        movement_y = movementXPower;*/
     }
 }
