6c89ec993f73c250d3b647275ccc41c3d4f79150
pandabear100 <nelsonysun@gmail.com>
2020-06-18 19:22:34 -0700

    Test class for the purposes of demonstrating pushing to SourceTree.

---
+++ Team6220_2019/src/main/java/org/firstinspires/ftc/team6220_2019/TestOpModes/TestClassNelson.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.team6220_2019.TestOpModes;
+
+public class TestClassNelson {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
